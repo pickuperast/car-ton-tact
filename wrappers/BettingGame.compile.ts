5aa14a92904fbf7ead20ae5f833082e2b695857c
Zhunisali <zshanabek@gmail.com>
2025-02-12 22:33:35 +0500

    racing game

--- wrappers/BettingGame.compile.ts
+++ wrappers/BettingGame.compile.ts
@@ -2,7 +2,7 @@ import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'tact',
-    target: 'contracts/betting_game.tact',
+    target: 'contracts/racing_game.tact',
     options: {
         debug: true,
     },
